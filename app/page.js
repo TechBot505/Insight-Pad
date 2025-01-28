d59e6cff8cd61417b0a3fbad58a02d64aac013e2
Rohit <rohitdhanotia2003@gmail.com>
2025-01-28 11:23:09 +0530

    Saving the users to Convex Db

--- app/page.js
+++ app/page.js
@@ -1,10 +1,32 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api"
 
 export default function Home() {
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    user && CheckUser();
+  }, [user]);
+
+  const CheckUser = async () => {
+    const result = await createUser({
+      userName: user?.fullName,
+      email: user?.primaryEmailAddress?.emailAddress,
+      imageUrl: user?.imageUrl
+    })
+    console.log(result);
+  }
+
   return (
     <div>
       <h2>Hello World!</h2>
       <Button>Click me</Button>
+      <UserButton />
     </div>
   );
 }
